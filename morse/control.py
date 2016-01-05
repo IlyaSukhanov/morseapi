49ad3d69f0ed3ab9950bbe673ec5e83030e7216b
Ilya Sukhanov <ilya@sukhanov.net>
2016-01-05 13:21:20 -0500

    Simplify byte array conversion

--- morse/control.py
+++ morse/control.py
@@ -1,8 +1,17 @@
 import pygatt.backends
 import time
+import logging
+import webcolor
 
 DEVICE_HANDLE = 19
-COMMANDS={"neck":0x03, "eye":0x09, "left_ear":0x0b, "right_ear":0x0c, "head":0x0d}
+COMMANDS={
+    "neck_light":0x03,
+    "eye_brightness":0x08,
+    "eye":0x09,
+    "left_ear":0x0b,
+    "right_ear":0x0c,
+    "head":0x0d
+}
 
 class WonderControl(object):
     def __init__(self, address):
@@ -10,15 +19,25 @@ class WonderControl(object):
         adapter.start()
         self.device = adapter.connect(address, address_type='random')
 
-    def command(self, command_name, command_value):
-        #message = bytearray(COMMANDS[command_name])
-        #message.append(command_value)
-        message = bytearray([COMMANDS[command_name]]) + command_value
-        print([hex(byte) for byte in message])
+    def command(self, command_name, command_values):
+        message = bytearray([COMMANDS[command_name]]) + command_values
+        logging.debug([hex(byte) for byte in message])
         self.device.char_write_handle(DEVICE_HANDLE, message)
 
-    def eye(self, value1, value2):
-        self.command("eye", bytearray([value1, value2]))
+    def one_byte_array(value):
+        return bytearray(struct.pack(">B", value)))
+
+    def two_byte_array(value):
+        return bytearray(struct.pack(">H", value)))
+
+    def eye(self, value):
+        self.command("eye", two_byte_array(value))
+
+    def eye_brightness(self, value):
+        self.command("eye_brightness", one_byte_array(value))
+
+    def neck_light(self, value):
+        self.command("neck_light", one_byte_array(value))
 
 if __name__ == "__main__":
     wc = WonderControl("C0:F0:84:3C:51:FA")
