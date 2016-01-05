28b541ba2ea23f79c6da0a17e07f4b4dcb519e90
Milen Pavlov <milen.pavlov@gmail.com>
2016-01-05 15:03:28 -0500

    use blink_duration instead of frequency to avoid fractions

--- morse/examples/clock.py
+++ morse/examples/clock.py
@@ -10,11 +10,11 @@ def draw_now(bot):
     now = datetime.datetime.now()
     return draw_time(bot, now.hour, now.minute)
 
-def draw_time(bot, hour, minute, blink_frequency=1):
+def draw_time(bot, hour, minute, blink_duration__seconds=1):
     ampm = "am" if hour < 12 else "pm"
     hour_bit = hour % 12
     minute_bit = minute * 12 / 60
-    refresh_interval = 0.5 / blink_frequency
+    refresh_interval = blink_duration__seconds / 2.0
 
     hour_value = (1 << hour_bit)
     minute_value = (1 << minute_bit)
@@ -25,7 +25,10 @@ def draw_time(bot, hour, minute, blink_frequency=1):
     bot.eye(hour_value | minute_value)
     time.sleep(refresh_interval)
 
-if __name__ == "__main__":
+def run():
     bot = WonderControl(DOT)
     while True:
         draw_now(bot)
+
+if __name__ == "__main__":
+    run()
